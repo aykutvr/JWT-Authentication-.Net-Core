c196294958a3b576d14bae83083e4a25f57b416b
Aykut Vuruşkaner <aykut.vuruskaner@gmail.com>
2022-02-08 13:35:18 +0300

    .

--- JWTAuthentication/Program.cs
+++ JWTAuthentication/Program.cs
@@ -9,11 +9,7 @@ builder.Services.AddSingleton<JWTAuthentication.Services.IUserService, JWTAuthen
 
 //--------------------------------------------------------------------------------------------------------------
 //JWT Authentication Service Implementation With Extension Method
-builder.Services.UseCustomJWTAuthentication<JWTAuthentication.Dto.UserDto>(config =>
-{
-    config.SetSecretKey("B?E(H+MbQeThWmZq4t7w9z$C&F)J@NcRfUjXn2r5u8x/A%D*G-KaPdSgVkYp3s6v9y$B&E(H+MbQeThWmZq4t7w!z%C*F-JaNcRfUjXn2r5u8x/A?D(G+KbPeSgVkYp3s6v9y$B&E)H@McQfTjWmZq4t7w!z%C*F-JaNdRgUkXp2r5u8x/A?D(G+KbPeShVmYq3t6v9y$B&E)H@McQfTjWnZr4u7x!z%C*F-JaNdRgUkXp2s5v8y/B?D(G+KbPeS");
-    config.SetJWTIssuer("https://www.github.com/");
-});
+builder.Services.UseCustomJWTAuthentication();
 //--------------------------------------------------------------------------------------------------------------
 
 builder.Services.AddControllersWithViews();
--- JWTAuthentication/Settings.cs
+++ JWTAuthentication/Settings.cs
@@ -2,8 +2,7 @@
 {
     public class Settings
     {
-        internal static string SecretKey { get; set;} = "";
-        internal static string JWTIssuer { get; set; } = "";
-        internal static Type UserDataObjectType { get; set; } = null;
+        internal const string SecretKey = "B?E(H+MbQeThWmZq4t7w9z$C&F)J@NcRfUjXn2r5u8x/A%D*G-KaPdSgVkYp3s6v9y$B&E(H+MbQeThWmZq4t7w!z%C*F-JaNcRfUjXn2r5u8x/A?D(G+KbPeSgVkYp3s6v9y$B&E)H@McQfTjWmZq4t7w!z%C*F-JaNdRgUkXp2r5u8x/A?D(G+KbPeShVmYq3t6v9y$B&E)H@McQfTjWnZr4u7x!z%C*F-JaNdRgUkXp2s5v8y/B?D(G+KbPeS";
+        internal const string JWTIssuer = "https://www.github.com/";
     }
 }
